9ab5800dc4de2f522354fb4d6bdca74163f847ab
Syed Hammad <amazonian1971@gmail.com>
2025-09-30 13:13:02 +0530

    Fix: Remove authOptions export for Vercel build

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,8 @@
 // src/app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-console.log("SECRET LENGTH:", process.env.NEXTAUTH_SECRET?.length);
-console.log("SECRET VALUE:", process.env.NEXTAUTH_SECRET);
 
-export const authOptions = {
+const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -40,8 +38,6 @@ export const authOptions = {
     error: "/",
   },
   secret: process.env.NEXTAUTH_SECRET,
-};
-
-const handler = NextAuth(authOptions);
+});
 
 export { handler as GET, handler as POST };
